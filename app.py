1872a4905bf1240bd814e9bce41ab3d640617984
jinhyeon kim <zhspt234@naver.com>
2022-01-09 19:20:18 +0900

    01/09 DB구축
    POST완성입니다

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 client = MongoClient('localhost', 27017)
 db = client.sparta_3team
 
-doc = {'name':'sea_elephant'}
-db.users.insert_one(doc)
+# doc = {'name': 'sea_elephant'}
+# db.users.insert_one(doc)
 
 
 ## URL 별로 함수명이 같거나,
@@ -16,41 +16,35 @@ db.users.insert_one(doc)
 
 @app.route('/')
 def home():
-   return render_template('index.html')
+    return render_template('index.html')
 
 
-@app.route('/test', methods=['GET'])
-def test_get():
-   title_receive = request.args.get('title_give')
-   print(title_receive)
-   return jsonify({'result':'success', 'msg': '이 요청은 GET!'})
+@app.route('/review', methods=['POST'])  # DB작성(이름, 리뷰, 별점)
+def review_POST():
+
+   name_recieve = request.form['name_give'] # name_recieve 로 클라이언트가 준 name 가져오기
+   review_recieve = request.form['review_give'] # review_recieve 로 클라이언트가 준 review 가져오기
+   star_recieve = request.form['star_give'] # star -- 로 클라이언트가 준 별점 가져오기
+
+   doc = {'name':name_recieve,
+          'review':review_recieve,
+          'star':star_recieve
+          }
+   db.cafereview.insert_one(doc) # db에 name, review, star 저장
 
 
-@app.route('/test', methods=['POST'])
-def test_post():
-   title_receive = request.form['title_give']
-   print(title_receive)
-   return jsonify({'result':'success', 'msg': '이 요청은 POST!'})
+   return jsonify({'msg': '리뷰가 성공적으로 작성되었습니다.'})
+
+@app.route('/review', methods=['GET'])
+def review_get():
+
+    reviews = list(db.cafereview.find({},{'_id': False}))
+
+    return jsonify({'all_reviews': reviews})
+
+
 
 
 if __name__ == '__main__':
-   app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
 
-# GEt, Post 요청 확인 ajax 코드입니다.
-#      }$.ajax({
-# #     type: "GET",
-# #     url: "/test?title_give=봄날은간다",
-# #     data: {},
-# #     success: function(response){
-# #        console.log(response)
-# #
-#   })
-# $.ajax({
-#     type: "POST",
-#     url: "/test",
-#     data: { title_give:'봄날은간다' },
-#     success: function(response){
-#        console.log(response)
-#     }
-#   })
-# 깃허브 테스트
